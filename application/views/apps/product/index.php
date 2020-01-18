4b5e1e002b267a01e4f88f9df10711acf26297e8
sovanrom <yeansovanrom8@gmail.com>
2020-01-18 10:20:29 +0700

    a

--- application/views/apps/product/index.php
+++ application/views/apps/product/index.php
@@ -1,12 +1,4 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
-<style type="text/css">
-    .table th:nth-child(0) { text-align: center; }
-    .table td:nth-child(0) { text-align: center; }
-    .table th:nth-child(3) { text-align: center; }
-    .table td:nth-child(3) { text-align: center; }
-    .table th:nth-child(4) { text-align: center; }
-    .table td:nth-child(4) { text-align: center; }
-</style>
 <section class="content">
     <div class="row">
         <div class="col-xs-12">
