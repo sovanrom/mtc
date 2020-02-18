7a122e3c4620e620213e017b928a9297c81919d5
sovanrom <yeansovanrom8@gmail.com>
2020-02-18 16:45:15 +0700

    a

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ date_default_timezone_set('Asia/Phnom_Penh');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:8888/mtc/';
+$config['base_url'] = 'http://localhost/mtc/';
 
 /*
 |--------------------------------------------------------------------------
